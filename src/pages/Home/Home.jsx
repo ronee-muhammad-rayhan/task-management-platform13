c54a9e850b5493cac34164092d7f79ecd98a57ea
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-12-22 18:35:21 +0600

    target people section added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -49,9 +49,52 @@ const Home = () => {
             </section> */}
 
             {/* Target User Section */}
-            <section className='py-12'>
-                {/* <TargetUser /> */}
-            </section>
+            <motion.div
+                initial={{ opacity: 0.6 }}
+                whileHover={{
+                    scale: 1.2,
+                    transition: { duration: 1 },
+                }}
+                whileTap={{ scale: 0.9 }}
+                whileInView={{ opacity: 1 }}
+
+            // className="bg-red-600 w-10 h-10 rounded-[20px]"
+            >
+                <section className='py-20'>
+                    {/* <TargetUser /> */}
+                    <section className="bg-gray-100 text-gray-800">
+                        <div className="container p-6 mx-auto space-y-6 text-center lg:p-8 lg:space-y-8">
+                            <h2 className="text-3xl font-bold">Target People Who will be Benefited with</h2>
+                            <div className="flex flex-wrap justify-center lg:justify-between">
+                                <div className="flex p-4 space-x-4 rounded-lg md:space-x-6 bg-gray-50 text-gray-800">
+                                    <p className="text-4xl font-bold leadi lg:text-6xl">Engineers</p>
+                                </div>
+                                <div className="flex p-4 space-x-4 rounded-lg md:space-x-6 bg-gray-50 text-gray-800">
+                                    <p className="text-4xl font-bold leadi lg:text-6xl">Doctors</p>
+                                </div>
+                                <div className="flex p-4 space-x-4 rounded-lg md:space-x-6 bg-gray-50 text-gray-800">
+                                    <p className="text-4xl font-bold leadi lg:text-6xl">Teachers</p>
+                                </div>
+                                <div className="flex p-4 space-x-4 rounded-lg md:space-x-6 bg-gray-50 text-gray-800">
+                                    <p className="text-4xl font-bold leadi lg:text-6xl">Students</p>
+                                </div>
+                                <div className="flex p-4 space-x-4 rounded-lg md:space-x-6 bg-gray-50 text-gray-800">
+                                    <p className="text-4xl font-bold leadi lg:text-6xl">Developers</p>
+                                </div>
+                                <div className="flex p-4 space-x-4 rounded-lg md:space-x-6 bg-gray-50 text-gray-800">
+                                    <p className="text-4xl font-bold leadi lg:text-6xl">Lawyers</p>
+                                </div>
+                                <div className="flex p-4 space-x-4 rounded-lg md:space-x-6 bg-gray-50 text-gray-800">
+                                    <p className="text-4xl font-bold leadi lg:text-6xl">Bankers</p>
+                                </div>
+                                <div className="flex p-4 space-x-4 rounded-lg md:space-x-6 bg-gray-50 text-gray-800">
+                                    <p className="text-4xl font-bold leadi lg:text-6xl">Others</p>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </section>
+            </motion.div>
         </div>
     );
 };
